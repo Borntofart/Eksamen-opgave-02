8b2aed6e75df9eeade2a1ec14c3ec062201b5e89
borntofart <the.real.beltner@gmail.com>
2026-05-25 19:16:29 +0200

    lavet arraylist og contact class samt phonebook

---
+++ src/Contact.java
@@ -0,0 +1,15 @@
+public class Contact {
+
+    //attributer
+    String name;
+    String PhoneNumber;
+
+//constructor
+    Contact(String name, String PhoneNumber) {
+        this.name = name;
+        this.PhoneNumber = PhoneNumber;
+
+    }
+
+
+}
---
+++ src/PhoneBook.java
@@ -0,0 +1,9 @@
+//skal impotere arrayliste det er sådan her
+import java.util.ArrayList;
+
+
+public class PhoneBook {
+//laver arraylisten husk den skal være private eller public jeg laver den public fordi jeg gerne vil have fat i den i Main
+public ArrayList<Contact> contacts = new ArrayList<>();
+
+}
